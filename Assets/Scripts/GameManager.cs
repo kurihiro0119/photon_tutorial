2d57a834a97c19f2970a6122ac75d6c39b1f7ebd
kurihiro0119 <54324711+kurihiro0119@users.noreply.github.com>
2021-01-30 12:09:52 +0900

    second commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Com.MyCompany.MyGame{
+
+
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+        // Start is called before the first frame update
+        public override void OnLeftRoom(){
+            SceneManager.LoadScene(0);
+        }
+
+        public void LeaveRoom(){
+            PhotonNetwork.LeaveRoom();
+        }
+
+        void LoadArena(){
+            if(!PhotonNetwork.IsMasterClient){
+                Debug.LogError("PhotonNetwork : Trying vel to Load a level but we are not the master Client");
+            }
+            Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);
+        }
+
+        public override void OnPlayerEnteredRoom(Player other){
+            Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName);
+
+            if(PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom ISMasterClient {0}", PhotonNetwork.IsMasterClient);
+
+                LoadArena();
+            }
+        }
+
+        public override void OnPlayerLeftRoom(Player other){
+            Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName);
+
+            if(PhotonNetwork.IsMasterClient){
+                Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient);
+
+                LoadArena();
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerNameInputField.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+
+namespace Com.MyCompany.MyGame{
+
+    [RequireComponent(typeof(InputField))]
+    public class PlayerNameInputField : MonoBehaviour
+    {
+        const string playerNamePrefKey = "PlayerName";
+        // Start is called before the first frame update
+        void Start()
+        {
+            string defaultName = string.Empty;
+            InputField _inputField = this.GetComponent<InputField>();
+            if(_inputField != null){
+                if(PlayerPrefs.HasKey(playerNamePrefKey)){
+                    defaultName =  PlayerPrefs.GetString(playerNamePrefKey);
+                    _inputField.text = defaultName;
+                }
+            }
+
+            PhotonNetwork.NickName = defaultName;
+        }
+
+        public void SetPlayerName(string value)
+        {
+            //Important
+            if(string.IsNullOrEmpty(value)){
+                Debug.LogError("Player Name is null or empty");
+                return;
+            }
+            PhotonNetwork.NickName = value;
+
+            PlayerPrefs.SetString(playerNamePrefKey, value);
+        }
+    }
+}
