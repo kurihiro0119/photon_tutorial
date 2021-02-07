c0b9464d3f025926d2f8957a72c8117fec163126
栗原啓成 <kuriharayoshinari@kuriharayoshinarinoMac-mini.local>
2021-02-07 20:51:11 +0900

    一通り完了

--- Assets/PunBasics_tutorial/Scripts/PlayerManager.cs
+++ Assets/PunBasics_tutorial/Scripts/PlayerManager.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using Photon.Pun;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 
 namespace Com.MyCompany.MyGame
 {
-    public class PlayerManager : MonoBehaviourPunCallbacks
+
+
+    public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
     {
         #region Private Fields
         [Tooltip("The Beams Gameobject to control")]
         [SerializeField]
         private GameObject beams;
+
+        [Tooltip("The Player's UI GameObject Prefab")]
+        [SerializeField]
+        private GameObject playerUiPrefab;
+        #endregion
+
+        #region PublicField
 
         [Tooltip("The current Health of our player")]
         public float Health = 1f;
@@ -20,15 +30,26 @@ namespace Com.MyCompany.MyGame
         //True, when the user is firing
         bool IsFiring;
 
+        [Tooltip("THe local player instance. Use this to know if the local player is represented in the Scene")]
+        public static GameObject LocalPlayerInstance;
+
         #endregion
 
-        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info){
 
-            if(stream.IsWriting){
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+
+            if (stream.IsWriting)
+            {
+                // このプレイヤーを所有しています。データをほかのものに送ります。
                 stream.SendNext(IsFiring);
+                stream.SendNext(Health);
             }
-            else{
+            else
+            {
+                // ネットワークプレイヤー。データ受信
                 this.IsFiring = (bool)stream.ReceiveNext();
+                this.Health = (float)stream.ReceiveNext();
             }
         }
 
@@ -47,34 +68,60 @@ namespace Com.MyCompany.MyGame
             {
                 beams.SetActive(false);
             }
+
+            if (photonView.IsMine)
+            {
+                PlayerManager.LocalPlayerInstance = this.gameObject;
+            }
+
+            DontDestroyOnLoad(this.gameObject);
         }
 
-        void Start(){
-            Debug.Log("Start!!!!!!!!!");
-            CameraWork _cameraWork = this.gameObject.GetComponent<CameraWork>();
+        public void Start()
+        {
+            CameraWork _cameraWork = gameObject.GetComponent<CameraWork>();
 
-            if(_cameraWork != null){
-                if(photonView.IsMine){
-                    Debug.Log("photon is mine!!!!!!!!");
+            if (_cameraWork != null)
+            {
+                if (photonView.IsMine)
+                {
                     _cameraWork.OnStartFollowing();
                 }
-                else{
-                    Debug.Log("isMine ではない");
-                }
             }
-            else{
-                Debug.LogError("<Color=Red><a>Missing</a></Color> CameraWork Component on playerPrefab.", this);
+            else
+            {
+                Debug.LogError("<Color=Red><b>Missing</b></Color> CameraWork Component on player Prefab.", this);
             }
+
+            if(playerUiPrefab!= null)
+            {
+                GameObject _uiGo = Instantiate(playerUiPrefab);
+                _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
+
+            }
+            else
+            {
+                Debug.LogWarning("<Color=Red><a>Missing</a></Color> PlayerUiPrefab reference on player Prefab.", this);
+            }
+
+#if UNITY_5_4_OR_NEWER
+            // Unity 5.4 has a new scene management. register a method to call CalledOnLevelWasLoaded.
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded += (scene, loadingMode) =>
+            {
+                this.CalledOnLevelWasLoaded(scene.buildIndex);
+            };
+            #endif
+
+
         }
+
 
         /// <summary>
         /// MonoBehaviour method called on GameObject by Unity on every frame.
         /// </summary>
         void Update()
         {
-            if(photonView.IsMine){
-                ProcessInputs();
-            }
+            ProcessInputs();
 
             //trigger Beams active state
             if (beams != null && IsFiring != beams.activeInHierarchy)
@@ -136,6 +183,22 @@ namespace Com.MyCompany.MyGame
                 return;
             }
             Health -= 0.1f * Time.deltaTime;
+        }
+
+        void OnLevelWasLoaded(int level)
+        {
+            this.CalledOnLevelWasLoaded(level); 
+        }
+
+        void CalledOnLevelWasLoaded(int level)
+        {
+            if(!Physics.Raycast(transform.position, -Vector3.up, 5f))
+            {
+                transform.position = new Vector3(0f, 5f, 0f);
+            }
+
+            GameObject _uiGo = Instantiate(this.playerUiPrefab);
+            _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
         }
     }
 
---
+++ Assets/PunBasics_tutorial/Scripts/PlayerUI.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Com.MyCompany.MyGame
+{
+
+
+    public class PlayerUI : MonoBehaviour
+    {
+        #region Private Field
+
+        [Tooltip("UI Text to display Player's Name")]
+        [SerializeField]
+        private Text playerNameText;
+
+        [Tooltip("UI Slider to dispaley Player's Health")]
+        [SerializeField]
+        private Slider playerHealthSlider;
+
+        private PlayerManager target;
+
+        float characterControllerHeight = 0;
+        Transform targetTransform;
+        Renderer targetRenderer;
+        CanvasGroup _canvasGroup;
+        Vector3 targetPosition;
+
+        #endregion
+
+        #region Public Field
+        [Tooltip("Pixel offset from the player target")]
+        [SerializeField]
+        private Vector3 screenOffset = new Vector3(0f, 30f, 0f);
+
+        #endregion
+
+        #region Public Field
+        public void SetTarget(PlayerManager _target)
+        {
+            if(_target == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> PlayMakerManager target for PlayerUI.SetTarget.", this);
+                return;
+            }
+
+            target = _target;
+            targetTransform = this.target.GetComponent<Transform>();
+            targetRenderer = this.target.GetComponent<Renderer>();
+            CharacterController characterController = _target.GetComponent<CharacterController>();
+
+            if(characterController != null)
+            {
+                characterControllerHeight = characterController.height;
+            }
+
+            if(playerNameText != null)
+            {
+                playerNameText.text = target.photonView.Owner.NickName;
+            }
+        }
+
+        #endregion
+
+        #region
+
+        private void Awake()
+        {
+            _canvasGroup = this.GetComponent<CanvasGroup>();
+            this.transform.SetParent(GameObject.Find("Canvas").GetComponent<Transform>(), false);
+        }
+
+        private void Update()
+        {
+            if(playerHealthSlider != null)
+            {
+                playerHealthSlider.value = target.Health;
+            }
+
+            if(target == null)
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if(targetRenderer != null)
+            {
+                this._canvasGroup.alpha = targetRenderer.isVisible ? 1f : 0f;
+            }
+
+            if(targetTransform != null)
+            {
+                targetPosition = targetTransform.position;
+                targetPosition.y += characterControllerHeight;
+                this.transform.position = Camera.main.WorldToScreenPoint(targetPosition) + screenOffset;
+            }
+        }
+
+        #endregion
+    }
+}
